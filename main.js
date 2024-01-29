3002c0a5610b55a3cabd70ca1bc36c17dbe786b1
LAPTOP-MN1KK15K\jakel <duckhangtuong@gmail.com>
2024-01-29 10:09:30 +0700

    Add custom namespace dialog

--- main.js
+++ main.js
@@ -1,27 +1,54 @@
 const codeGenerator = require("./core/generator");
-const fs = require('fs');
+const fs = require("fs");
+
+let info = {
+  diagram: null,
+  filePath: null,
+  namespace: null
+};
 
 function init() {
-    app.commands.register("csharp.entity:generate", generateCode, "Generate entities");
+  app.commands.register(
+    "csharp.entity:generate",
+    generateCode,
+    "Generate entities"
+  );
 }
 
 function generateCode() {
-    app.elementPickerDialog.showDialog("Select an ER Diagram", null, type.ERDDiagram).then(function ({buttonId, returnValue}) {
-        if (buttonId === 'ok') {
-            const diagram = returnValue;
-            app.dialogs.showInputDialog("Enter export file path").then(function ({buttonId, returnValue}) {
-                if (buttonId === 'ok') {
-                  const filePath = returnValue;
-                  if (!fs.existsSync(filePath)) {
-                      window.alert("File path is not exist!");
-                      return;
-                  }
-                  
-                  codeGenerator.generateCode(diagram, filePath);
-                } 
-            })
-        }
-    })
+  app.elementPickerDialog
+    .showDialog("Select an ER Diagram", null, type.ERDDiagram)
+    .then(payload => handleSelectDiagram(payload));
+}
+
+function handleSelectDiagram({ buttonId, returnValue }) {
+  if (buttonId === "ok") {
+    info.diagram = returnValue;
+    app.dialogs
+      .showInputDialog("Enter export file path")
+      .then(payload => handleExportFilePath(payload));
+  }
+}
+
+function handleExportFilePath({ buttonId, returnValue }) {
+  if (buttonId === "ok") {
+    info.filePath = returnValue;
+    if (!fs.existsSync(info.filePath)) {
+      window.alert("File path is not exist!");
+      return;
+    }
+
+    app.dialogs
+      .showInputDialog("Enter namespace")
+      .then(payload => handleNamespace(payload));
+  }
+}
+
+function handleNamespace({ buttonId, returnValue }) {
+  if (buttonId === "ok") {
+    info.namespace = returnValue;
+    codeGenerator.generateCode(info);
+  }
 }
 
 exports.init = init;
